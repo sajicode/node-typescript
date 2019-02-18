ac77e864304ee18e27bc076708286896e293a816
sajicode <sajioloye@gmail.com>
2019-02-18 22:22:28 +0100

    set up routes

---
+++ src/routes/index.ts
@@ -0,0 +1,28 @@
+import * as express from "express";
+
+export const register = (app: express.Application) => {
+	const oidc = app.locals.oidc;
+
+	// define a route handler for the default home page
+	app.get("/", (req: any, res) => {
+		const user = req.userContext ? req.userContext.userinfo : null;
+		res.render("index", { isAuthenticated: req.isAuthenticated(), user });
+	});
+
+	// define a secure route handler for the login page that redirects to /games
+	app.get("/login", oidc.ensureAuthenticated(), (req, res) => {
+		res.redirect("/games");
+	});
+
+	// define logout route
+	app.get("/logout", (req: any, res) => {
+		req.logout();
+		res.redirect("/");
+	});
+
+	// define secure route handler for games page
+	app.get("/games", oidc.ensureAuthenticated(), (req: any, res) => {
+		const user = req.userContext ? req.userContext.userinfo : null;
+		res.render("games", { isAuthenticated: req.isAuthenticated(), user });
+	});
+};
