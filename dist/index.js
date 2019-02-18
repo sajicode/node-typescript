2ffde2cff0e0f3f65514f1d6364c76741a08b701
sajicode <sajioloye@gmail.com>
2019-02-18 14:28:08 +0100

    set up tools for views and update scripts in package.json

--- dist/index.js
+++ dist/index.js
@@ -4,11 +4,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const path_1 = __importDefault(require("path"));
 const app = express_1.default();
 const port = 8080;
+// configure express to use EJS
+app.set("views", path_1.default.join(__dirname, "views"));
+app.set("view engine", "ejs");
 // route handler for default home page
 app.get("/", (req, res) => {
-    res.send("Hello World!!");
+    // render the index template
+    res.render("index");
 });
 // start the express server
 app.listen(port, () => {
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 import express from "express";
+import path from "path";
 const app = express();
 
 const port = 8080;
 
+// configure express to use EJS
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
 // route handler for default home page
 app.get("/", (req, res) => {
-	res.send("Hello World!!");
+	// render the index template
+	res.render("index");
 });
 
 // start the express server
